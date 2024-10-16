a4bf0ad8aea089be55b2b1f79cc13afa699061a5
scotiana2415 <scotiana2004@gmail.com>
2024-10-16 07:29:15 +0300

    ex4

--- Lab1_MAP_bun/src/ThirdExercise.java
+++ Lab1_MAP_bun/src/ThirdExercise.java
@@ -1,20 +1,20 @@
-import java.util.Arrays;
+import java.util.Arrays; // Needed for the Array toString conversion
 
 public class ThirdExercise {
-    public int[] add(int[] addToEndOne, int[] addToEndTwo) {
-        int aux = 0;
-        int[] result = new int[addToEndOne.length];
-        for (int i = addToEndOne.length - 1; i >= 0; i--) {
-            int sum = addToEndOne[i] + addToEndTwo[i] + aux;
+
+    public int[] add(int[] addendOne, int[] addendTwo) {
+        int carry = 0;
+        int[] result = new int[addendOne.length];
+        for (int i = addendOne.length - 1; i >= 0; i--) {
+            int sum = addendOne[i] + addendTwo[i] + carry;
             result[i] = sum % 10;
-            aux = sum / 10;
+            carry = sum / 10;
         }
-        if (aux > 0) {
-            int[] extendedResult = new int[addToEndOne.length + 1];
-            for (int i = 0; i < result.length; i++) {
+        if (carry > 0) {
+            int[] extendedResult = new int[addendOne.length + 1];
+            for (int i = 0; i < result.length; i++)
                 extendedResult[i + 1] = result[i];
-            }
-            extendedResult[0] = aux;
+            extendedResult[0] = carry;
             return extendedResult;
         }
         return result;
@@ -28,11 +28,79 @@ public class ThirdExercise {
             if (diff < 0) {
                 diff += 10;
                 borrow = 1;
-            } else
+            } else {
                 borrow = 0;
+            }
             result[i] = diff;
         }
         return result;
     }
 
+    public int[] multiply(int[] multiplicand, int multiplier) {
+        int carry = 0;
+        int[] result = new int[multiplicand.length];
+        for (int i = multiplicand.length - 1; i >= 0; i--) {
+            int mul = multiplicand[i] * multiplier + carry;
+            result[i] = mul % 10;
+            carry = mul / 10;
+        }
+        if (carry > 0) {
+            int[] extendedResult = new int[multiplicand.length + 1];
+            for (int i = 0; i < result.length; i++)
+                extendedResult[i + 1] = result[i];
+            extendedResult[0] = carry;
+            return extendedResult;
+        }
+        return result;
+    }
+
+    public int[] divide(int[] dividend, int divisor) {
+        if (divisor == 0) {
+            throw new IllegalArgumentException("Divisor cannot be zero.");
+        }
+        if (dividend == null || dividend.length == 0) {
+            return new int[]{0};
+        }
+
+        int[] result = new int[dividend.length];
+        int remainder = 0;
+        for (int i = 0; i < dividend.length; i++) {
+            int current = remainder * 10 + dividend[i];
+            result[i] = current / divisor;
+            remainder = current % divisor;
+        }
+
+        // Remove leading zeros
+        int nonZeroIndex = 0;
+        while (nonZeroIndex < result.length && result[nonZeroIndex] == 0) {
+            nonZeroIndex++;
+        }
+
+        if (nonZeroIndex == result.length) {
+            return new int[]{0};
+        }
+
+        int[] perfectedResult = new int[result.length - nonZeroIndex];
+        for (int i = 0; i < perfectedResult.length; i++) {
+            perfectedResult[i] = result[nonZeroIndex + i];
+        }
+
+        return perfectedResult;
+    }
+
+    public static void main(String[] args) {
+        ThirdExercise exercise = new ThirdExercise();
+        int[] firstNumber = {1, 3, 0, 0, 0, 0, 0, 0, 0};
+        int[] secondNumber = {8, 7, 0, 0, 0, 0, 0, 0, 0};
+        int[] thirdNumber = {8, 3, 0, 0, 0, 0, 0, 0, 0};
+        int[] fourthNumber = {5, 4, 0, 0, 0, 0, 0, 0, 0};
+        int[] fifthNumber = {2, 3, 6, 0, 0, 0, 0, 0, 0};
+
+        System.out.println("Third Exercise:");
+        System.out.println("1. [1, 3, 0, 0, 0, 0, 0, 0, 0] + [8, 7, 0, 0, 0, 0, 0, 0, 0] = " + Arrays.toString(exercise.add(firstNumber, secondNumber)));
+        System.out.println("2. [8, 3, 0, 0, 0, 0, 0, 0, 0] - [5, 4, 0, 0, 0, 0, 0, 0, 0] = " + Arrays.toString(exercise.subtract(thirdNumber, fourthNumber)));
+        System.out.println("3. [2, 3, 6, 0, 0, 0, 0, 0, 0] * 2 = " + Arrays.toString(exercise.multiply(fifthNumber, 2)));
+        System.out.println("4. [2, 3, 6, 0, 0, 0, 0, 0, 0] / 2 = " + Arrays.toString(exercise.divide(fifthNumber, 2)));
+        System.out.println("");
+    }
 }
